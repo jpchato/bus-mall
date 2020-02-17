c7bd8c6965fef9664139e483fba8e3780e431c9b
Jesse Pena <jpchato@gmail.com>
2020-02-17 13:51:17 -0800

    added javascript code, unsure if it's working

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var firstImage = document.getElementById('first-image');
+var renderedImage1 = null;
+var imageIndex1 = null;
+var secondImage = document.getElementById('second-image');
+var renderedImage2 = null;
+var imageIndex2 = null;
 var allBusMallImages = [];
 var totalClicks = 0;
 
@@ -29,3 +35,55 @@ new BusMallImage('unicorn', './img/unicorn.jpg');
 new BusMallImage('usb', './img/usb.gif');
 new BusMallImage('water-can', './img/water-can.jpg');
 new BusMallImage('wine-glass', './img/wine-glass.jpg');
+
+function getRandomImage () {
+  var randomIndex = Math.floor(Math.random() * allBusMallImages.length);
+
+  while (
+    renderedImage1 === allBusMallImages[randomIndex].name ||
+    renderedImage2 === allBusMallImages[randomIndex].name
+  ){
+    randomIndex = Math.floor(Math.random() * allBusMallImages.length)
+  }
+  return randomIndex;
+}
+
+function handleImageClick (event) {
+  var imageId = event.target.getAttribute('alt');
+  console.log(imageId);
+
+  for (var i = 0; i < allBusMallImages.length; i++) {
+    if (imageId === allBusMallImages[i].name) {
+      allBusMallImages[i].timesClicked++;
+    }
+  }
+  if (totalClicks < 5) {
+    totalClicks++;
+    imageIndex1 = getRandomImage();
+    renderedImage1 = allBusMallImages[imageIndex1].name;
+    imageIndex2 = getRandomImage();
+    renderImages();
+  } else {
+    alert('Thank you for your help. Your input is valuable to us!')
+  }
+  console.log(allBusMallImages);
+}
+
+function renderImages () {
+  firstImage.setAttribute('src', allBusMallImages[imageIndex1].image);
+  firstImage.setAttribute('src', allBusMallImages[imageIndex1].name);
+  allBusMallImages[imageIndex1].timesRendered++;
+  secondImage.setAttribute('src', allBusMallImages[imageIndex2].image);
+  secondImage.setAttribute('src', allBusMallImages[imageIndex2].name);
+  allBusMallImages[imageIndex2].timesRendered++;
+}
+
+firstImage.addEventListener('click', handleImageClick);
+secondImage.addEventListener('click', handleImageClick);
+
+imageIndex1 = getRandomImage();
+renderedImage1 = allBusMallImages[imageIndex1].name;
+imageIndex2 = getRandomImage();
+renderedImage2 = allBusMallImages[imageIndex2].name;
+
+renderImages();
